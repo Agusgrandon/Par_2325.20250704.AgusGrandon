73b90b35bc8b81ab8538e1d7654ca29d2cfbdf77
agus grandon <agustina107.grandon@gmail.com>
2025-07-04 19:01:25 -0300

    cree las clases

--- src/modelo/Cine.java
+++ src/modelo/Cine.java
@@ -1,10 +1,46 @@
 package modelo;
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  *
  * @author agus
+ * Cine: contiene lista de salas y entradas.
  */
 public class Cine implements Serializable{
+    private List<Sala> salas;
+    private List<Entrada> entradas;
+    private HashMap<String, Cliente> clientes;
+
+    public Cine(List<Sala> salas, List<Entrada> entradas, HashMap<String, Cliente> clientes) {
+        this.salas = salas;
+        this.entradas = entradas;
+        this.clientes = clientes;
+    }
+
+    public List<Sala> getSalas() {
+        return salas;
+    }
+
+    public List<Entrada> getEntradas() {
+        return entradas;
+    }
+
+    public HashMap<String, Cliente> getClientes() {
+        return clientes;
+    }
     
+    public void registrarClientes(Cliente cliente, String usuario){
+        clientes.put(usuario, cliente);
+    }
+    
+    public void agregarEntrada(Entrada entrada){
+        entradas.add(entrada);
+        entrada.getCliente().agregarEntrada(entrada);
+    }
+    
+    public String entradasString(Cliente cliente){
+    
+    }
 }
--- src/modelo/Cliente.java
+++ src/modelo/Cliente.java
@@ -1,13 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package modelo;
+import java.util.List;
 
 /**
  *
- * @author NISSI
+ * @author agus
  */
 public class Cliente {
+    private String nombre;
+    private String email;
+    private String contraseña;
+    private List<Entrada> listaEntradas;
+
+    public Cliente(String nombre, String email, String contraseña) {
+        this.nombre = nombre;
+        this.email = email;
+        this.contraseña = contraseña;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+    
     
 }
